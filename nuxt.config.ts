338eb172f176daa2d4cd6c53a4e89d62553e677f
Harrison <56010813+hthegoat@users.noreply.github.com>
2026-01-08 19:41:09 -0500

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,7 +40,15 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: 'Real-time surf reports. No ads. No bloat. No BS.' }
+        { name: 'description', content: 'Real-time surf reports. No ads. No bloat. No BS.' },
+        { name: 'theme-color', content: '#FACC15' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }
       ]
     }
   }
